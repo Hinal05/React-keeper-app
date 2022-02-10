1d72d6dbca3b70ed442465bc98b2b9d6eb72b406
Hinal05 <hinal.parikh@qed42.com>
2022-02-10 14:23:44 +0530

    main: Header, footer and note created.

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Header from './Header/header';
+import Footer from './Footer/footer';
+import Note from './Note/note';
+
+function App() {
+    return (
+        <section>
+            <Header />
+            <Note />
+            <Footer />
+        </section>
+    )
+}
+
+export default App;
---
+++ src/components/Header/header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header>
+            <h1>Keeper</h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
