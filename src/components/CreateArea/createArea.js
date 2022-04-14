2e1a4a870ed26571beab180610f5d57fdb5af0bf
Hinal05 <hinal.parikh@qed42.com>
2022-04-14 18:25:41 +0530

    Material design core, icon and form animation added.

--- src/components/CreateArea/createArea.js
+++ src/components/CreateArea/createArea.js
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+import Zoom from '@material-ui/core/Zoom';
 
 function CreateArea(props) {
 
+  const [isExpanded, setExpanded] = useState(false);
   const [note, setNote] = useState({
     title: "",
     content: ""
@@ -30,12 +34,20 @@ function CreateArea(props) {
     event.preventDefault();
   }
 
+  function expand() {
+    setExpanded(true);
+  }
+
   return (
     <div>
-      <form>
-        <input name="title" value={note.title} onChange={handleChange} placeholder="Title" />
-        <textarea name="content" value={note.content} onChange={handleChange} placeholder="Take a note..." rows="3" />
-        <button onClick={submitNote} >Add</button>
+      <form className="create-note">
+        {isExpanded && (
+          <input name="title" value={note.title} onChange={handleChange} placeholder="Title" />
+        )}
+        <textarea name="content" onClick={expand} value={note.content} onChange={handleChange} placeholder="Take a note..." rows={isExpanded ? 3 : 1} />
+        <Zoom in={isExpanded}>
+          <Fab onClick={submitNote}><AddIcon /></Fab>
+        </Zoom>
       </form>
     </div>
   );
